11545d489d06cd40533730b7cef13687535874d1
suryakant-thombre-au6 <suryathombre000@gmail.com>
2023-01-01 04:33:21 +0530

    lesson 5 chapter 6

--- zombieownership.sol
+++ zombieownership.sol
@@ -5,6 +5,9 @@ import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+  // 1. Define mapping here
+  mapping (uint => address) zombieApprovals;
+
   function balanceOf(address _owner) external view returns (uint256) {
     return ownerZombieCount[_owner];
   }
@@ -13,20 +16,20 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     return zombieToOwner[_tokenId];
   }
 
-  // Define _transfer() here
-  function _transfer(address _from, address _to, uint256 _tokenId)private{
-      ownerZombieCount[_to]++;
-      ownerZombieCount[_from]--;
-      zombieToOwner[_tokenId] = _to;
-      emit Transfer(_from, _to, _tokenId);
-
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to]++;
+    ownerZombieCount[_from]--;
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-
+    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+    _transfer(_from, _to, _tokenId);
   }
 
   function approve(address _approved, uint256 _tokenId) external payable {
 
   }
+
 }
